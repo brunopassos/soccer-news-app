5409c4b32d2605c21f4ebf8500dca0625d9356a4
Bruno <brunopassos_bp@hotmail.com>
2022-07-18 01:06:07 -0300

    Favorite icons

--- app/src/main/java/com/example/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/com/example/soccernews/ui/news/NewsFragment.java
@@ -1,6 +1,7 @@
 package com.example.soccernews.ui.news;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,13 +11,16 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.room.Room;
 
+import com.example.soccernews.data.local.AppDatabase;
 import com.example.soccernews.databinding.FragmentNewsBinding;
 import com.example.soccernews.ui.adapters.NewsAdapter;
 
 public class NewsFragment extends Fragment {
 
     private FragmentNewsBinding binding;
+    private AppDatabase db;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,10 +29,15 @@ public class NewsFragment extends Fragment {
 
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        db = Room.databaseBuilder(getContext(), AppDatabase.class, "soccer-news")
+                .allowMainThreadQueries()
+                .build();
 
         binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
         newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
-            binding.rvNews.setAdapter(new NewsAdapter(news));
+            binding.rvNews.setAdapter(new NewsAdapter(news, updatedNews -> {
+                //db.newsDao().insert(updatedNews);
+            }));
         });
         return root;
     }
